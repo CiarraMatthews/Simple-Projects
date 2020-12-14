76edc0d7ab574e74a6192d10e7aa35e0febb1159
CiarraMatthews <cdmatthews@ucdavis.edu>
2020-12-14 15:25:47 -0800

    Removed label and added different buttons for wage options

--- Affordability_Calculator/calculator.py
+++ Affordability_Calculator/calculator.py
@@ -2,8 +2,10 @@ import sys
 
 #Importing QApplication and all the required widgets
 from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QLabel
+#from PyQt5.QtWidgets import QLabel
 from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QHBoxLayout
+from PyQt5.QtWidgets import QPushButton 
 
 #instance of QApplication
 app = QApplication([])
@@ -11,10 +13,18 @@ app = QApplication([])
 #instance of application's GUI
 window = QWidget()
 window.setWindowTitle('Calculator')
-window.setGeometry(100, 100, 280, 80)
+
+#Layout
+layout = QHBoxLayout()
+layout.addWidget(QPushButton('By Hourly'))
+layout.addWidget(QPushButton('By Monthly'))
+layout.addWidget(QPushButton('By Salary'))
+window.setLayout(layout)
+
+window.setGeometry(800, 700, 780, 580)
 window.move(60, 15)
-message = QLabel('<h1>Hello World!</h1>', parent = window)
-message.move(60, 15)
+#message = QLabel('<h1>Hello World!</h1>', parent = window)
+#message.move(60, 15)
 
 #shows the application' GUI
 window.show()
