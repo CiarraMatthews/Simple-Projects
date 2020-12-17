9c86a2a3232c51445c6a43c436193c1c90af13f6
CiarraMatthews <cdmatthews@ucdavis.edu>
2020-12-16 22:19:31 -0800

    Fleshing out hourly pay function. Still need tax formula

--- Affordability_Calculator/functionality.py
+++ Affordability_Calculator/functionality.py
@@ -1,5 +1,9 @@
 #The app without the GUI
 
+#Constants
+DEFAULT = 33
+BAD_VAL = "Please enter a valid number"
+
 def by_net_income():
 	#Fix the taxes!
 	income = int(input("Please enter your income here: "))
@@ -10,7 +14,7 @@ def by_gross_income():
 	try:
 		income = int(input("Please enter your income here: "))
 	except ValueError:
-		print("Please enter a valid number")
+		print(BAD_VAL)
 	try:
 		choice = int(input("Press 1 for default rent percentage of 33%, 2 to chose your own percentage "))
 	except ValueError:
@@ -25,8 +29,16 @@ def by_gross_income():
 
 def by_hourly():
 	#Fix taxes. Take from net when done
-	income = int("Please enter your income here: ")
-
+	try:
+		wages = int(input("What is your hourly wage: "))
+	exceptValue:
+		print(BAD_VAL)
+	try:
+		hours = int(input("How many hours do you work per pay period:  "))
+	exceptValue:
+		print(BAD_VAL)
+	try:
+		pay_rate = input("")
 
 def by_yearly():
 	#Check for accuracy
@@ -35,15 +47,25 @@ def by_yearly():
 
 def percentages():
 	try:
-		percent = int(input("Please input a value between 10 and 40%: "))
+		choice = int(input("Press 1 for the default value, and 2 for custom"))
 	except ValueError:
 		print("Please enter a valid number")
+	if choice == 1:
+		percent = DEFAULT
+	elif choice == 2:
+		try:
+			percent = int(input("Please input a value between 10 and 40%: "))
+		except ValueError:
+			print(BAD_VAL)
+	else:
+		print(BAD_VAL)
 	return percent/100
 
 def find_taxes():
 	pass
 
 def main():
+	print("This app calculates the amount of rent you can afford based on your income.")
 	print("Press 1 to calculate by monthly net income \nPress 2 to calculate by monthly gross income \nPress 3 to start calculating by hourly pay \nPress 4 to calculate by yearly pay", end="")
 	choice = int(input(": "))
 	if choice == 1:
