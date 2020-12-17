88ad4a1c8ffff634eaaa2c3cbef76da768a1cacd
CiarraMatthews <cdmatthews@ucdavis.edu>
2020-12-17 15:57:29 -0800

    Removed everything tool bar, added new display and button functions

--- Affordability_Calculator/calculator.py
+++ Affordability_Calculator/calculator.py
@@ -1,8 +1,9 @@
 import sys
 
+from PyQt5.QtCore import Qt
 #Importing QApplication and all the required widgets
-from PyQt5.QtWidgets import QApplication, QMainWindow, QStatusBar, QToolBar
-from PyQt5.QtWidgets import QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QStatusBar
+from PyQt5.QtWidgets import QLabel, QGridLayout, QVBoxLayout
 from PyQt5.QtWidgets import QWidget, QHBoxLayout, QPushButton
 from PyQt5.QtWidgets import QFormLayout, QLineEdit, QDialog, QDialogButtonBox
 
@@ -11,18 +12,46 @@ class CalcUI(QMainWindow):
 	def __init__(self, parent = None):
 		super().__init__(parent)
 		self.setWindowTitle('Calculator')
+		self.setFixedSize(435, 335)
+		self.generalLayout = QVBoxLayout()
+		self._centralWidget = QWidget(self)
+		self.setCentralWidget(self._centralWidget)
+		self._centralWidget.setLayout(self.generalLayout)
+#Display
+		self._createDisplay()
+		self._createButtons()
+
 		self._createMenu()
-		self._createToolBar()
 		self._createStatusBar()
+
+	def _createDisplay(self):
+		#Creating display widget
+		self.display = QLineEdit()
+		#Setting display properties
+		self.display.setFixedHeight(35)
+		self.display.setAlignment(Qt.AlignRight)
+		self.display.setReadOnly(True)
+		self.generalLayout.addWidget(self.display)
+
+	def _createButtons(self):
+		self.buttons = {}
+		buttonsLayout = QGridLayout()
+		#Button text and layout
+		buttons = {'By Hourly': (0,0),
+					'By Net': (2,0),
+					'By Gross': (3,0),
+					'By Salary': (4,0),
+			}
+		for btnText, pos in buttons.items():
+			self.buttons[btnText] = QPushButton(btnText)
+			self.buttons[btnText].setFixedSize(40, 40)
+			buttonsLayout.addWidget(self.buttons[btnText], pos[0], pos[1])
+		self.generalLayout.addLayout(buttonsLayout)
 
 	def _createMenu(self):
 		self.menu = self.menuBar().addMenu("&Menu")
 		self.menu.addAction('Exit', self.close)
 
-	def _createToolBar(self):
-		tools = QToolBar()
-		self.addToolBar(tools)
-		tools.addAction('Exit', self.close)
 
 	def _createStatusBar(self):
 		status = QStatusBar()
